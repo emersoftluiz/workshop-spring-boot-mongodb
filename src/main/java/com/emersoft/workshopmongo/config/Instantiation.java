03a702319d79769357561963b70038e7cedc8adf
Emersoft <emersonbsi@gmail.com>
2020-07-05 22:36:34 -0300

    Proteção dos dados do Author com DTO

--- src/main/java/com/emersoft/workshopmongo/config/Instantiation.java
+++ src/main/java/com/emersoft/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.emersoft.workshopmongo.domain.Post;
 import com.emersoft.workshopmongo.domain.User;
+import com.emersoft.workshopmongo.dto.AuthorDTO;
 import com.emersoft.workshopmongo.repository.PostRepository;
 import com.emersoft.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
